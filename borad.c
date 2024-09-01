5848be32dbcc6235ad76435911a65639b8069a2b
NokEeGoi <somchaizzzz@hotmail.com>
2024-09-01 18:10:29 +0700

    borad

---
+++ borad.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void display_borad();
+
+int main(){
+
+    display_borad();
+}
+
+void display_borad(){
+    
+    printf ("      |          |       \n");
+    printf ("-------------------------\n");
+    printf ("      |          |       \n");
+    printf ("-------------------------\n");
+    printf ("      |          |       \n");
+ 
+    
+}
+
